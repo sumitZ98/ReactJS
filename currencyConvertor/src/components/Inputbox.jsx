66123caaffcb4b57ef44cea1ac8a7de624f84b3f
Sumit <sumitmitra98@gmail.com>
2024-03-06 12:28:01 +0530

    changes made

--- currencyConvertor/src/components/Inputbox.jsx
+++ currencyConvertor/src/components/Inputbox.jsx
@@ -1,70 +1,59 @@
-import PropTypes from 'prop-types';
+import { useId } from "react";
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+// import PropTypes from 'prop-types';
 function InputBox({
-    label,
-    amount,
-    onAmountChange,
-    onCurrencyChange,
-    currencyOption = [],
-    selectCurrency = "usd",
-    amountDisable = false,
-    currencyDisable = false,
-    className = "" 
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
 }) {
-   
-
-    return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
-            <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
-                   { label}
-                </label>
-                <input
-                    
-                    className="outline-none w-full bg-transparent py-1.5"
-                    type="number"
-                    placeholder="Amount"
-                    disabled={amountDisable}
-                    value={amount}
-                    onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
-                />
-            </div>
-            <div className="w-1/2 flex flex-wrap justify-end text-right">
-                <p className="text-black/40 mb-2 w-full">Currency Type</p>
-                <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    disabled={currencyDisable}
-                    value={selectCurrency}
-                    onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value)}
-                >
-                    
-                        <option value="usd">
-                            usd
-                        </option>
-                
-                </select>
-            </div>
-        </div>
-    );
+  const amountInputId = useId();
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
+        <input
+          id={amountInputId}
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          disabled={currencyDisable}
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+          {currencyOptions.map((currency) => (
+            // eslint-disable-next-line react/jsx-key
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 }
-
-InputBox.propTypes = {
-    label: PropTypes.string.isRequired,
-    amount: PropTypes.number.isRequired,
-    onAmountChange: PropTypes.func.isRequired,
-    onCurrencyChange: PropTypes.func.isRequired,
-    currencyOption: PropTypes.arrayOf(PropTypes.string),
-    selectCurrency: PropTypes.string,
-    amountDisable: PropTypes.bool,
-    currencyDisable: PropTypes.bool,
-    className: PropTypes.string
-};
-
-InputBox.defaultProps = {
-    currencyOption: [],
-    selectCurrency: "usd",
-    amountDisable: false,
-    currencyDisable: false,
-    className: ""
-};
 
 export default InputBox;
