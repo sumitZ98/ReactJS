8e04327f83cf3c4304ac3072270b1624cd9aa7dc
Sumit <sumitmitra98@gmail.com>
2024-03-20 12:26:36 +0530

    commited

---
+++ useContext/src/components/UserContextProvider.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import UserContext from "../context/UserContext";
+
+// eslint-disable-next-line react/prop-types
+const UserContextProvider = ({children}) => {
+    const [user,setUser] = useState(null)
+    return(
+        <UserContext.Provider value={{user,setUser}}>
+        {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider;
---
+++ useContext/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import React from "react";
+//creating context....
+const UserContext = React.createContext();
+
+export default UserContext;
+//we also have to create a provider
